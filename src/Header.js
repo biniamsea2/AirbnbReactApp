b165f08b4fc96575e35ec435f31e6404dc095283
Biniam <Biniamt23@yahoo.com>
2021-03-25 22:25:23 -0700

    material-ui added

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './Header.css';
+import Search from "@material-ui/icons/Search";
+
 
 function Header() {
     return (
         <div className="header">
             <img className ="header_logo"src="https://www.wfla.com/wp-content/uploads/sites/71/2020/03/AIRBNB.jpg" alt="airbnb logo"/>
+        
+        
+        <div className="header_center">
+            <input type="text"/>
+            <Search/>
+
+        </div>
+        
+        
+        
         </div>
     )
 }
