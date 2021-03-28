e57f363c131ccef889e6e013e4be817d45e85285
Biniam <Biniamt23@yahoo.com>
2021-03-27 23:59:26 -0700

    almost got banner fixed2

--- src/Banner.js
+++ src/Banner.js
@@ -7,14 +7,13 @@ import {Button} from "@material-ui/core";
 function Banner() {
     return (
         <div className="banner">
-            {/* <div className="banner_info">
+            <div className="banner_info">
                 <h2>Get out and stretch your imagination </h2>
                 <h5>Get out and stretch your imagination </h5>
-                <Button variant='outlined'>Explore Nearby</Button>
-            </div> */}
+                // <Button variant='outlined'>Explore Nearby</Button>
+            </div>
         </div>
     )
 }
 
 export default Banner
-
