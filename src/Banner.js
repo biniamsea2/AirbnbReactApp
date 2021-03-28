96fc9a94c748601d9e270f82afe16c8599a8e4dc
Biniam <Biniamt23@yahoo.com>
2021-03-28 01:39:16 -0700

    everything working with css, changed button css, just need to find transparent white logo

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./Banner.css";
 import "./Button.css";
-import {Button} from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import image1 from "./Images/1.jpg";
 
 
