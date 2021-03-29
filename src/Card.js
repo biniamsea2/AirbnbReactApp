173add4229578dcc99016df648a9fb3894625b0a
Biniam <Biniamt23@yahoo.com>
2021-03-29 03:01:31 -0700

    css for cards added

--- src/Card.js
+++ src/Card.js
@@ -5,6 +5,12 @@ function Card({src, title, description,price}) {
     return (
         <div className="card">
             <img src={src} alt="card"/>
+            <div className="card_info">
+                <h2 Style="color:black;">{title}</h2>
+                <h4 Style="color:black">{description}</h4>
+                <h3>{price}</h3>
+
+            </div>
             
         </div>
     )
