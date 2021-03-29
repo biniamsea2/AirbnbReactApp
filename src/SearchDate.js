5c86fd4d8d169795d2c41d0a57cb8e5679a4b747
Biniam <Biniamt23@yahoo.com>
2021-03-29 02:11:25 -0700

    search airbnb button css done

--- src/SearchDate.js
+++ src/SearchDate.js
@@ -30,14 +30,19 @@ function SearchDate() {
       {/* pass in the array object, then onchange we pass in the handleSelect function (which sets the dates) */}
       <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
       <div>
-        <h1 className="guest">Guests: <People/><br></br><input min={1} type="number" placeholder="Add guests" /></h1>
-        
-        <Button Style="color: #ff7779; background:white; font-weight:bold; margin-top:5px; margin-left:440px;">
+        <h1 className="guest">
+          Guests: <People />
+          <br></br>
+          <input min={1} type="number" placeholder="Add guests" />
+        </h1>
+
+        <Button id="guestButton" >
           Search Airbnb
         </Button>
       </div>
     </div>
   );
 }
+// margin-top:5px;
 
 export default SearchDate;
