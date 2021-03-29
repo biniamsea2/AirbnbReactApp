3532f22fe5809f393150f9a2e0e878cb542e01cc
Biniam <Biniamt23@yahoo.com>
2021-03-28 20:55:21 -0700

    everything up to date with button and css for searchDates

--- src/Banner.js
+++ src/Banner.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React from "react";
 import "./Banner.css";
 import "./Button.css";
 import { Button } from "@material-ui/core";
 import image1 from "./Images/1.jpg";
 
-
 function Banner() {
-    return (
-        <div className="banner">
-            <div className="banner_info">
-                <img id="backgroundImg" src={image1} alt=""></img>
-                <h1>Get out and stretch your 
-                    imagination </h1>
-                <h5>
-                    Plan a different kind of getaway to
-                    uncover the hidden gems near you. </h5>
-                <Button>Explore Nearby</Button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="banner">
+      <div className="banner_search">
+        <Button className="banner_searchButton" variant="outlined">
+          Search Dates
+        </Button>
+      </div>
+      <div className="banner_info">
+        <img id="backgroundImg" src={image1} alt=""></img>
+        <h1>Get out and stretch your imagination </h1>
+        <h5>
+          Plan a different kind of getaway to uncover the hidden gems near you.{" "}
+        </h5>
+        <Button>Explore Nearby</Button>
+      </div>
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;
