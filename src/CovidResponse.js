18d1977b6a90ceccf940691d94f420c1aa9fa3ce
Biniam <Biniamt23@yahoo.com>
2021-03-29 19:49:15 -0700

    covid response message opens with new tab now

--- src/CovidResponse.js
+++ src/CovidResponse.js
@@ -4,7 +4,9 @@ import './CovidResponse.css';
 function CovidResponse() {
   return (
     <div className="covidResponse">
-      <a href="https://www.airbnb.com/resources/hosting-homes?persona=guest">Get the latest on our COVID-19 response</a>
+      {/* rel is not required but is a secruity recommendation */}
+      {/* allows us to open with a new tab: */}
+      <a target="_blank" rel="noopener noreferrer" href="https://www.airbnb.com/resources/hosting-homes?persona=guest">Get the latest on our COVID-19 response</a>
     </div>
   );
 }
