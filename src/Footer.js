0ab2774119c72673267d7079729d0067c108c72e
Biniam <Biniamt23@yahoo.com>
2021-03-30 22:12:52 -0700

    everything with footer complete and css and positioning working great

--- src/Footer.js
+++ src/Footer.js
@@ -1,17 +1,68 @@
 import React from "react";
 import "./Footer.css";
-import LanguageIcon from '@material-ui/icons/Language';
+import LanguageIcon from "@material-ui/icons/Language";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
 
 function Footer() {
   return (
     <div>
       <div className="footer">
-        <p>© 2021 Airbnb Demo</p> 
-        <button><a href="https://www.airbnb.com/help/article/2855/privacy-policy">Privacy</a></button> 
-        <button><a href="https://www.airbnb.com/help/article/2908/terms-of-service">Terms</a></button> 
-        <button><a href="https://www.airbnb.com/sitemaps/v2">Sitemap</a></button>
+        <p className="airbnb_demo">© 2021 Airbnb Demo</p>
+        <button>
+          <a href="https://www.airbnb.com/help/article/2855/privacy-policy">
+            Privacy
+          </a>
+        </button>
+        <button>
+          <a href="https://www.airbnb.com/help/article/2908/terms-of-service">
+            Terms
+          </a>
+        </button>
+        <button>
+          <a href="https://www.airbnb.com/sitemaps/v2">Sitemap</a>
+        </button>
 
-        <LanguageIcon fontSize="small"/>
+        <div className="test">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/airbnb"
+          >
+            <TwitterIcon className="footerRight" style={{ color: "black" }} />{" "}
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.instagram.com/airbnb/"
+          >
+            <InstagramIcon className="footerRight" style={{ color: "black" }} />{" "}
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com/airbnb"
+          >
+            <FacebookIcon className="footerRight" style={{ color: "black" }} />{" "}
+          </a>
+
+          <p className="footerRight">$ USD</p>
+          <div className="footerRight">
+            <p className="footerRight">English (US)</p>
+          </div>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.airbnb.com/"
+          >
+            <LanguageIcon
+              className="footerRight"
+              fontSize="small"
+              style={{ color: "black" }}
+            />{" "}
+          </a>
+        </div>
       </div>
     </div>
   );
