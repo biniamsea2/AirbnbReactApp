2ae13a225cd9e5526f231a5725da3aec50a49677
Biniam <Biniamt23@yahoo.com>
2021-03-31 20:33:14 -0700

    changed src tag to img tag

--- src/Home.js
+++ src/Home.js
@@ -12,22 +12,22 @@ function Home() {
       <h1>Live anywhere</h1>
       <div className="home_section">
         <Card
-          src="https://a0.muscache.com/im/pictures/7d82ca14-56e5-4465-8218-dcfa7d69b6ac.jpg?im_w=720"
+          img="https://a0.muscache.com/im/pictures/7d82ca14-56e5-4465-8218-dcfa7d69b6ac.jpg?im_w=720"
           title="Entire homes"
           description=""
         />
         <Card
-          src="https://a0.muscache.com/im/pictures/36f53e61-db8d-403c-9122-5b761c0e4264.jpg?im_w=720"
+          img="https://a0.muscache.com/im/pictures/36f53e61-db8d-403c-9122-5b761c0e4264.jpg?im_w=720"
           title="Unique stays"
           description=""
         />
         <Card
-          src="https://a0.muscache.com/im/pictures/a596c528-a2e9-4417-9bbe-ceb68b2f6b9c.jpg?im_w=720"
+          img="https://a0.muscache.com/im/pictures/a596c528-a2e9-4417-9bbe-ceb68b2f6b9c.jpg?im_w=720"
           title="Cabins and cottages"
           description=""
         />
         <Card
-          src="https://a0.muscache.com/im/pictures/10a638e1-6aff-4313-8033-1275cec83987.jpg?im_w=720"
+          img="https://a0.muscache.com/im/pictures/10a638e1-6aff-4313-8033-1275cec83987.jpg?im_w=720"
           title="Pets allowed"
           description=""
         />
@@ -47,7 +47,7 @@ function Home() {
       <div className="home_section">
         <Card
           Style="position:relative; z-index:-1;"
-          src={card2}
+          img={card2}
           title=""
           description=""
         />
@@ -58,17 +58,17 @@ function Home() {
       </h3>
       <div className="home_section">
         <Card
-          src="https://a0.muscache.com/im/pictures/ad109d56-2421-40cd-98e6-e114160dc85b.jpg?im_w=1200"
+          img="https://a0.muscache.com/im/pictures/ad109d56-2421-40cd-98e6-e114160dc85b.jpg?im_w=1200"
           title="Experiences"
           description="Local things to do, wherever you are."
         />
         <Card
-          src="https://a0.muscache.com/im/pictures/a6b08861-feb8-4a01-a76d-b33d2867b441.jpg?im_w=1200"
+          img="https://a0.muscache.com/im/pictures/a6b08861-feb8-4a01-a76d-b33d2867b441.jpg?im_w=1200"
           title="Online Experiences"
           description="Live, interactive activities led by Hosts."
         />
         <Card
-          src="https://a0.muscache.com/im/pictures/247a1ea3-946d-4eb8-a6ab-e8b9a66846f4.jpg?im_w=1200"
+          img="https://a0.muscache.com/im/pictures/247a1ea3-946d-4eb8-a6ab-e8b9a66846f4.jpg?im_w=1200"
           title="Featured collection: Wanderlust"
           description="Travel from home with Online Experiences."
         />
