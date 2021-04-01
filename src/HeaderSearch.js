582aca792fad82519cf96d1b013bb376af68feda
Biniam <Biniamt23@yahoo.com>
2021-03-31 18:42:46 -0700

    created new header for search page

---
+++ src/HeaderSearch.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HeaderSearch() {
+    return (
+        <div>
+            <p>Hello from header search 2</p>
+            <img src ="" alt ="logo"/>
+        </div>
+    )
+}
+
+export default HeaderSearch
